5d3f95445baa34b4f65f04347507c1c377f5cdc2
Arijit Basu <sayanarijit@gmail.com>
2022-11-15 17:31:40 +0530

    Add flake

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Scan a QR code in the terminal using the system camera or a given image";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.url = "github:domenkozar/nix/relaxed-flakes";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, nix, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          qrscan = pkgs.rustPlatform.buildRustPackage rec {
+            name = "qrscan";
+            src = ./.;
+
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+
+            buildInputs = [ pkgs.stdenv.cc.libc ];
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+        }
+      );
+      defaultPackage = forAllSystems (system: self.packages.${system}.qrscan);
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          devRequirements = with pkgs; [
+            gcc
+            gnumake
+            clippy
+            rustc
+            cargo
+            rustfmt
+            rust-analyzer
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            RUST_BACKTRACE = 1;
+
+            buildInputs = devRequirements;
+            packages = devRequirements;
+          };
+        }
+      );
+    };
+}
